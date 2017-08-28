b88c83d9ead923879c30faef10755ff165c94d5f
Adi Zhavo <adi@gram.gs>
2017-08-28 15:39:17 +0100

    Update project dependencies and main

---
+++ Google_Sheet_Exporter/GoogleSheet2JsonApplication.cs
@@ -0,0 +1,14 @@
+﻿namespace GoogleSheet2Json
+{
+    public class GoogleSheet2JsonApplication
+    {
+        static void Main(string[] args)
+        {
+            var parser = new Parser();
+            var lexer = new Lexer(parser);
+            var sheet2Json = new GoogleSheet2Json(args, lexer);
+            sheet2Json.ExtractData();
+            sheet2Json.WriteData();
+        }
+    }
+}
