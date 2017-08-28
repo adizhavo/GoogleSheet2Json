40b5d470501b4b4493d70970bc35b205f599b908
Adi Zhavo <adi@gram.gs>
2017-08-28 12:17:24 +0100

    Create assembly

---
+++ Google_Sheet_Exporter/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+// Information about this assembly is defined by the following attributes.
+// Change them to the values specific to your project.
+
+[assembly: AssemblyTitle("GoogleSheet2Json")]
+[assembly: AssemblyDescription("Application for exporting data from google sheet to json")]
+[assembly: AssemblyCopyright("Adi Zhavo")]
+
+// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
+// The form "{Major}.{Minor}.*" will automatically update the build and revision,
+// and "{Major}.{Minor}.{Build}.*" will update just the revision.
+
+[assembly: AssemblyVersion("1.0.0")]
+
