3f153baeb8030b010af5c47595f5d8c7f8d5ce8a
Adi Zhavo <adi@gram.gs>
2017-09-23 17:42:52 +0100

    Add config reader to read the config file

---
+++ GoogleSheet2Json/Config/AppConfig.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace GoogleSheet2Json
+{   
+    public class AppConfig
+    {
+        public string applicationName;
+        public string spreadSheetId;
+        public string userName;
+        public string clientSecret;
+        public string outputDirectory;
+    }
+
+    public class ConfigReader
+    {
+        public AppConfig appConfig { private set; get; }
+        
+        public void Initialise(ExportConfig exportConfig)
+        {
+            if (string.IsNullOrEmpty(exportConfig.configPath))
+            {
+                throw new ArgumentNullException("[ConfigReader] config path is empty");
+            }
+            
+            StreamReader file =  new StreamReader(exportConfig.configPath);
+            using (file)
+            {
+                appConfig = JsonConvert.DeserializeObject<AppConfig>(file.ReadToEnd());
+                if (!string.IsNullOrEmpty(exportConfig.outputDir))
+                {
+                    appConfig.outputDirectory = exportConfig.outputDir;
+                }
+            }
+        }
+    }
+}
