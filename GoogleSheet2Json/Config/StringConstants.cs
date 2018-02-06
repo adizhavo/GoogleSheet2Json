4bb4133a0e325426dff77084e32492c21f014c1e
Adi Zhavo <adi@gram.gs>
2018-02-06 12:04:58 +0000

    Fix regex pattern to ignore \-* chars

--- GoogleSheet2Json/Config/StringConstants.cs
+++ GoogleSheet2Json/Config/StringConstants.cs
@@ -6,7 +6,7 @@
     
     public class StringConstants
     {
-        public const string WORD_PATTERN = @"(\w+\s*)*[^( \[* | \]* | \(* | \)* | \>* | \-* | \,* | *)]";
+        public const string WORD_PATTERN = @"(\w+\s*)*[^( \[* | \]* | \(* | \)* | \>* | \,* | *)]";
         public const string EMPTY_SPACE_PATTERN = @"^\s";
         public const string COMMA = ",";
         public const string RANGE_CHAR = ">";
